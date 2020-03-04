0681e08e2dab3f4bd62753f92d08682951444d51
khoironi <masrony37@gmail.com>
2020-03-04 20:40:17 +0700

    parallax

---
+++ script.js
@@ -0,0 +1,48 @@
+// parallax
+
+$(window).on('load', function () {
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+
+
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+    // console.log(wScroll);
+
+    //header
+    $(' .header img').css({
+        'transform': 'translate(0px, ' + wScroll / 2.9 + '%)'
+    });
+
+    $(' .header h2').css({
+        'transform': 'translate(0px, ' + wScroll / 1.8 + '%)'
+    });
+    $(' .header h5').css({
+        'transform': 'translate(0px, ' + wScroll / 0.7 + '%)'
+    });
+
+
+
+    //experience
+    if (wScroll > $('.experience').offset().top - 550) {
+        $('.experience .gambar').addClass('munculkan');
+        console.log('ok');
+    };
+
+    //services
+    if (wScroll > $('.line1').offset().top - 400) {
+        // console.log('ok');
+
+        $('.line1 .colline1').each(function (i) {
+            setTimeout(function () {
+                $('.line1 .colline1').eq(i).addClass('muncul');
+            }, 300 * (i + 1));
+        });
+        // 
+    }
+
+
+
+
+});
